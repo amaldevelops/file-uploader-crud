585cf857e943acef6a943efe5a45e1f79273d47e
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-01-20 14:01:07 +1100

    Add code to populate database

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,8 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function main() {
+  console.log("Seed to Database");
+
+  const saveFileDetails = await prisma.test.create({
+    data: {
+      id: "1",
+      name: "2025 Calendar",
+    },
+  });
+}
+
+main()
+  .catch((error) => {
+    throw error;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
 export async function getHome(req, res, next) {
   res.render("index");
 }
 
-export async function getFileUpload(req,res,next)
-{
-    res.render("uploadFiles");
+export async function getFileUpload(req, res, next) {
+  res.render("uploadFiles");
 }
--- routes/appRouter.js
+++ routes/appRouter.js
@@ -4,10 +4,14 @@ import { getHome } from "../controllers/AppController.js";
 
 import { getFileUpload } from "../controllers/AppController.js";
 
+import { main } from "../controllers/AppController.js";
+
 const AppRouter = Router();
 
 AppRouter.get("/", getHome);
 
 AppRouter.get("/uploadfiles",getFileUpload )
+
+AppRouter.get("/seed",main)
 
 export default AppRouter;
