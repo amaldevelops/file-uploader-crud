dff35d3ec471eb12f1acfc8b54ea672b203c3c24
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-01-20 12:33:54 +1100

    Add Upload and notAuthorized Views EJS code

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,3 +1,8 @@
 export async function getHome(req, res, next) {
   res.render("index");
 }
+
+export async function getFileUpload(req,res,next)
+{
+    res.render("uploadFiles");
+}
--- routes/appRouter.js
+++ routes/appRouter.js
@@ -2,8 +2,12 @@ import { Router } from "express";
 
 import { getHome } from "../controllers/AppController.js";
 
+import { getFileUpload } from "../controllers/AppController.js";
+
 const AppRouter = Router();
 
 AppRouter.get("/", getHome);
+
+AppRouter.get("/uploadfiles",getFileUpload )
 
 export default AppRouter;
