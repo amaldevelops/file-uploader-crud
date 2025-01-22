7de4958ffb877033754ab8398ba25952aced9d68
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-01-23 09:03:14 +1100

    Fixed Sign In Form validator

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -2,15 +2,14 @@ import { Router } from "express";
 
 import { getHome } from "../controllers/AppController.js";
 
+import { ValidateUserLogin } from "../validators/signInFormValidation.js";
+
 import {
   getFileUpload,
   authenticateUser,
   getNotAuthorized,
-  formValidationSignIn
-  
+  formValidationSignIn,
 } from "../controllers/AppController.js";
-
-// import { authenticateUser } from "../controllers/AppController.js";
 
 import { passportInstance } from "../security/passportConfig.js";
 import passport from "passport";
@@ -22,7 +21,9 @@ AppRouter.get("/", getHome);
 // AppRouter.get("/uploadfiles", getFileUpload);
 
 AppRouter.post(
-  "/uploadfiles",formValidationSignIn,
+  "/uploadfiles",
+  ValidateUserLogin,
+  formValidationSignIn,
   passport.authenticate("local", { failureRedirect: "/notauthorized" }),
   authenticateUser
 );
--- validators/signInFormValidation.js
+++ validators/signInFormValidation.js
@@ -3,7 +3,7 @@ import { body, validationResult } from "express-validator";
 const emailError = "Must be an E-mail address as username";
 
 const ValidateUserLogin = [
-  body("username")
+  body("user_name")
     .trim()
     .notEmpty()
     .escape()
